25cfd0fdd5c4773aa0e55b8a6b326f400a58357a
Alexander <krasnoyurchenko.a.s@gmail.com>
2019-05-30 09:53:35 +0400

    upd AppCarousels

--- src/components/WorksItem.vue
+++ src/components/WorksItem.vue
@@ -15,28 +15,28 @@
 	}
 
 	.image-box {
-		display: block;
+		display: flex;
 		color: var(--color);
 		text-decoration: none;
 		position: relative;
-		margin: 9px 0 0 9px;
+		margin: range(3px, 9px) 0 0 range(3px, 9px);
+		padding: scale(20px, 0.5);
 
 		&::before {
 			content: '';
 			display: block;
 			padding-top: percentage(356 / 686);
-			background-color: #a7a7a7;
+			background-color: inherit;
 		}
 
 		&::after {
 			content: '';
 			position: absolute;
-			z-index: -1;
-			top: -9px;
-			right: range(9px, 40px);
-			bottom: range(9px, 20px);
-			left: -9px;
-			background-color: inherit;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: rgba(0, 0, 0, 0.5);
 		}
 
 		&:empty {
@@ -44,9 +44,17 @@
 		}
 	}
 
+	.image-underlay {
+		position: absolute;
+		top: range(-3px, -9px);
+		right: range(5px, 40px);
+		bottom: range(5px, 20px);
+		left: range(-3px, -9px);
+		background-color: inherit;
+	}
+
 	.image {
 		position: absolute;
-		z-index: 1;
 		top: 0;
 		left: 0;
 		width: 100%;
@@ -54,22 +62,35 @@
 		object-fit: cover;
 	}
 
+	.logo {
+		position: absolute;
+		z-index: 1;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		width: auto;
+		max-width: 60%;
+		max-height: 60%;
+		margin: auto;
+	}
+
 	.caption {
 		position: relative;
 		z-index: 1;
-		margin: range(-9px, -20px) range(9px, 40px) 0 9px;
-		padding: range(10px, 30px) range(15px, 35px) range(10px, 20px) range(15px, 55px);
+		margin: range(-5px, -20px) range(5px, 40px) 0 range(3px, 9px);
+		padding: range(10px, 30px) range(15px, 30px) range(10px, 20px) range(15px, 50px);
 	}
 
 	.description {
 		color: #ffffff;
 		font-family: var(--font-family);
-		font-size: range(1.6rem, 1.8rem);
+		font-size: range(1.5rem, 1.8rem);
 		line-height: 1.25;
 		overflow: hidden;
 		text-overflow: ellipsis;
 		height: calc(1.25em * 2);
-		margin-bottom: 1em;
+		margin: 0 0 1em;
 
 		>>> p {
 			margin: 0;
@@ -79,7 +100,7 @@
 	.title {
 		color: #ffffff;
 		font-family: var(--font-family);
-		font-size: range(1.4rem, 1.6rem);
+		font-size: range(1.3rem, 1.6rem);
 		line-height: 1.25;
 		margin: 0 0 1em;
 		overflow: hidden;
@@ -99,7 +120,7 @@
 		color: #ffffff;
 		text-decoration: none;
 		font-family: var(--font-family);
-		font-size: range(1.3rem, 1.5rem);
+		font-size: range(1.2rem, 1.5rem);
 		line-height: 1.25;
 		padding-bottom: 2px;
 		background: linear-gradient(to right, currentColor, currentColor) no-repeat 0 100% / 0 2px;
