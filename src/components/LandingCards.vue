7d1f6de51153f4e3a166f8bbb3073a756f423fff
Alexander <krasnoyurchenko.a.s@gmail.com>
2019-07-11 20:09:41 +0400

    upd LandingCards

--- src/components/LandingCards.vue
+++ src/components/LandingCards.vue
@@ -3,16 +3,45 @@
 		.grid
 			.grid-row
 				.grid-cell(
-					v-for="({tag, slots}, index) of cards"
+					v-for=`({
+						tag, slots: {
+							title,
+							description,
+							price,
+							'order-button': orderButton,
+							'more-link': moreLink
+						}
+					}, index) of cards`
 					:key="index"
 				)
 					.card(
 						:is="tag"
 					)
+						.card-body
+							.card-title(
+								v-if="title"
+								:is="title[0].tag"
+							) {{ getText(title[0].children) }}
+
+							.card-description(v-if="description")
+								v-nodes(:vnodes="description")
+
+							.card-price(v-if="price")
+								v-nodes(:vnodes="price[0].children")
+
+							base-button.card-order-button(v-if="orderButton") {{ getText(orderButton[0].children) }}
+
+							a.card-more-link(
+								v-if="moreLink"
+								v-bind="moreLink[0].data.attrs"
+							) {{ getText(moreLink[0].children) }}
 </template>
 
 <script>
-	import {getScopedSlot} from '../helpers';
+	import {
+		getScopedSlot,
+		getVNodesTextContent as getText
+	} from '../helpers';
 
 	export default {
 		name: 'LandingCards',
@@ -20,6 +49,9 @@
 			cards() {
 				return getScopedSlot(this.$scopedSlots.cards);
 			}
+		},
+		methods: {
+			getText
 		}
 	};
 </script>
@@ -33,6 +65,28 @@
 			box-sizing: border-box;
 			max-width: 100%;
 			flex-flow: column;
+		}
+	}
+
+	.grid-row {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.grid-cell {
+		box-sizing: border-box;
+		flex: 0 0 20%;
+		min-width: 0;
+	}
+
+	.card {
+		all: initial;
+
+		& {
+			display: flex;
+			box-sizing: border-box;
+			max-width: 100%;
+			height: 100%;
 		}
 	}
 </style>
