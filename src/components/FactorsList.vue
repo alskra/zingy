60e4d14dc3e1c8d9e8df64ba626890c958bacaf9
Alexander <krasnoyurchenko.a.s@gmail.com>
2019-07-19 16:48:42 +0400

    upd `FactorsList`

--- src/components/FactorsList.vue
+++ src/components/FactorsList.vue
@@ -20,17 +20,18 @@
 					.factor(
 						:is="tag"
 					)
-						.factor-header
-							.factor-title(
-								v-if="title"
-								:is="title[0].tag"
-							) {{ getText(title) }}
+						.factor-body
+							.factor-header
+								.factor-title(
+									v-if="title"
+									:is="title[0].tag"
+								) {{ getText(title) }}
 
-						.factor-main
-							.factor-description(v-if="description")
-								v-nodes(:vnodes="description")
+							.factor-main
+								.factor-description(v-if="description")
+									v-nodes(:vnodes="description")
 
-							.factor-number {{ index + 1 }}
+						.factor-number {{ index + 1 }}
 </template>
 
 <script>
@@ -86,7 +87,7 @@
 		box-sizing: border-box;
 		padding: range(5px, 20px);
 		/*flex: 0 0 percentage(1 / 3);*/
-		flex: 1 1 300px;
+		flex: 1 1 400px;
 		min-width: 0;
 	}
 
@@ -98,7 +99,6 @@
 			box-sizing: border-box;
 			padding: range(15px, 25px) range(10px, 30px);
 			height: 100%;
-			flex-flow: column;
 			color: #ffffff;
 			font-family: var(--font-family);
 			background-color: #2a2c2b;
@@ -112,6 +112,12 @@
 		}
 	}
 
+	.factor-body {
+		box-sizing: border-box;
+		flex: 0 1 100%;
+		min-width: 0;
+	}
+
 	.factor-header {
 		display: flex;
 		margin-bottom: range(10px, 20px);
@@ -122,11 +128,13 @@
 		box-sizing: border-box;
 		margin: 0 auto 0 range(-4px - 10px, -8px - 30px);
 		padding: range(4px, 8px) range(4px + 10px, 8px + 30px);
-		max-width: calc(100% + 2 * range(4px + 10px, 8px + 30px));
+		max-width: calc(100% + range(4px + 10px, 8px + 30px));
 		font-size: range(1.6rem, 1.8rem);
 		font-weight: 500;
 		line-height: 1.25;
 		background-color: var(--color-accent);
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 
 	.factor-main {
@@ -135,19 +143,19 @@
 
 	.factor-description {
 		box-sizing: border-box;
-		flex: 1 1 auto;
+		flex: 0 1 100%;
 		min-width: 0;
 		font-size: range(1.4rem, 1.6rem);
 		line-height: var(--line-height);
 
 		p {
-			margin: 0;
+			display: inline;
 		}
 	}
 
 	.factor-number {
 		box-sizing: border-box;
-		margin-left: range(10px, 30px);
+		margin: auto 0 auto range(10px, 30px);
 		flex: 0 0 auto;
 		min-width: 0;
 		font-size: range(3.8rem, 4.0rem);
