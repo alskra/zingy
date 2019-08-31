d0b1eced1c83dd2240c0ba78020a5b361ce10ce2
alskra <krasnoyurchenko.a.s@gmail.com>
2019-09-01 00:47:58 +0400

    test js optimize #20

--- vue.config.js
+++ vue.config.js
@@ -129,6 +129,16 @@ module.exports = {
 				]
 			}])
 			.after('preload');
+
+		if (config.plugins.has('prefetch')) {
+			config.plugin('prefetch').tap(options => {
+				options[0].fileBlacklist = options[0].fileBlacklist || [];
+				options[0].fileBlacklist.push(/\.css$/);
+
+				return options;
+			});
+		}
+
 		//
 		// if (config.plugins.has('prefetch')) {
 		// 	// config.plugin('prefetch').tap(options => {
